aa8774b97b352f6c54df17e7a11e77acbf56394c
wangjiannan <904089747@qq.com>
2017-04-11 21:24:37 +0800

    third

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -31,7 +31,16 @@ const webpackDev ={
         loader: 'file-loader'
       }
     ]
-  }
+  },
+  devServer: {
+        // contentBase: './build',//默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录（本例设置到"build"目录）
+        // colors: true,//在cmd终端中输出彩色日志
+        // historyApiFallback: true,//在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
+        // inline: true,//设置为true，当源文件改变时会自动刷新页面
+        // port: 8080,//设置默认监听端口，如果省略，默认为"8080"
+        // process: true,//显示合并代码进度
+        host : '0.0.0.0'
+    }
 }
 
 
