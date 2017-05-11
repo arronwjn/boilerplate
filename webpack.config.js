daef1e0a6800c1e2f5b502afd7527b689829ca8a
wangjiannan <904089747@qq.com>
2017-05-11 21:29:39 +0800

    add dirname

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports={
   devtool: 'source-map',//找到源代码错误
   entry:'./index.js',　　　//指定入口文件
   output:{
-    path:'build',　　　　　　//出口文件所在文件夹
+    path:__dirname + '/build',　　　　　　//出口文件所在文件夹
     filename:'bundle.js',　　//出口文件
     publicPath: 'build'  //公共路径
   },
